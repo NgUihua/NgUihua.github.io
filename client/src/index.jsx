fc9b91e906c6b5eff66983a5a050a15d95e0b68d
NgUihua <hwh1991@gmail.com>
2018-09-12 16:52:55 +0800

    :art:

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,9 +5,13 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes.jsx'
 import './styles/main.scss'
 import 'font-awesome/scss/font-awesome'
+import { sw } from './util/util'
 
 
 class Index extends Component {
+  componentWillMount() {
+    sw()
+  }
   render() {
     return (
       <Routes />
